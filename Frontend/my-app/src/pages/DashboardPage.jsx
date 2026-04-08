af3518b00bcf527f0fb8a2f5f7f221e43727e2d0
Stasa Dragovic <stasa.dr03@gmail.com>
2026-04-08 12:05:19 +0200

    feature(frontend): Added Navbar after login component

--- Frontend/my-app/src/pages/DashboardPage.jsx
+++ Frontend/my-app/src/pages/DashboardPage.jsx
@@ -1,27 +1,35 @@
+import  Sidebar  from "../components/layout/SideBar";
+import NavBarAfterLogin from "../components/layout/NavBarAfterLogin";
 import Button from "../components/ui/Button";
 import SearchBar from "../components/ui/SearchBar";
 
 const DashboardPage = () => {
   return (
-    <div className="p-8 font-sans">
-      <h1 className="text-2xl font-bold mb-6 text-[#111827]">Dashboard Page</h1>
+    <div className="flex h-screen">
+      <Sidebar />
+      
+      <div className="flex flex-col flex-1 overflow-hidden">
+        <NavBarAfterLogin username="" />
+        
+        <main className="p-8 overflow-y-auto">
+          <h1 className="text-2xl font-bold mb-6 text-[#111827]">Dashboard Page</h1>
 
-      {/* --- Visual Search Bar Test --- */}
-      <div className="mb-10">
-        <SearchBar placeholder="Search users..." />
-      </div>
+          <div className="mb-10">
+            <SearchBar placeholder="Search users..." />
+          </div>
 
-      {/* --- Buttons --- */}
-      <div className="flex flex-wrap gap-4 border-t pt-8">
-        <Button>Default</Button>
-        <Button variant="outline">Outline</Button>
-        <Button variant="secondary">Secondary</Button>
-        <Button variant="ghost">Ghost</Button>
-        <Button variant="destructive">Destructive</Button>
-        <Button variant="link">Link</Button>
+          <div className="flex flex-wrap gap-4 border-t pt-8">
+            <Button>Default</Button>
+            <Button variant="outline">Outline</Button>
+            <Button variant="secondary">Secondary</Button>
+            <Button variant="ghost">Ghost</Button>
+            <Button variant="destructive">Destructive</Button>
+            <Button variant="link">Link</Button>
+          </div>
+        </main>
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default DashboardPage;
