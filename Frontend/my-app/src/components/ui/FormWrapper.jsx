386cc2c028e901e86b36d4aff2f0235d6890d259
Vladimir Josic <vladimir.josic@symphony.com>
2026-04-09 15:53:10 +0200

    feature(frontent):Added FormatWrapper component and designed Register Page

---
+++ Frontend/my-app/src/components/ui/FormWrapper.jsx
@@ -0,0 +1,17 @@
+import { cn } from "../../utils/cn";
+
+const FormWrapper = ({ children, className }) => {
+  return (
+    <div
+      className={cn(
+        "w-full max-w-[540px] rounded-3xl border border-border bg-white p-8 shadow-sm gap-2",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default FormWrapper;
+
