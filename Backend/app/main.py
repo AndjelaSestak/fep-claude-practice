5dd11d7686c7ccac52ceab1a120397a6ae769bba
Lazar-Mrdjenovic <lazar.mrdjenovic@symphony.is>
2026-04-14 15:38:24 +0200

    feature(backend): Implementing login and creating access and refresh tokens

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,12 +1,35 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from alembic import command
+from alembic.config import Config
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers.auth import router as auth_router
-from app.routers.auth import router as auth_router
 from app.utils.errors import setup_exception_handlers
-from app.database import engine, Base, SessionLocal
 from app.seed import seed
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+  
+    alembic_cfg = Config("alembic.ini")
+    
+   
+    try:
+        command.downgrade(alembic_cfg, "base")
+    except Exception:
+        pass
+
+    command.upgrade(alembic_cfg, "head")
+    
+    try:
+        seed()
+    except Exception as e:
+        print(f"Failed to seed data: {e}")
+        
+
+    yield 
+    
+
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
@@ -16,21 +39,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(auth_router)
-
 setup_exception_handlers(app)
 
 app.include_router(auth_router)
-
-@app.on_event("startup")
-def startup_event():
-
-    Base.metadata.drop_all(bind=engine)
-    # Creating tables if they do not exist
-    Base.metadata.create_all(bind=engine)
-    
-    db = SessionLocal()
-    try:
-        seed()
-    finally:
-        db.close()
--- Backend/app/utils/security.py
+++ Backend/app/utils/security.py
@@ -13,7 +13,7 @@ REFRESH_TOKEN_EXPIRE_DAYS = 7
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return bcrypt.checkpw(plain_password.encode("utf-8"), hashed_password.encode("utf-8"))
 
-def hash_password(password: str) -> str:
+def get_password_hash(password: str) -> str:
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
 
 def create_access_token(data: dict) -> str:
