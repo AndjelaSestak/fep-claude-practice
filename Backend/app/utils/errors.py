7302b87019fd7024db93e769db44e77c8b29a774
Lazar-Mrdjenovic <lazar.mrdjenovic@symphony.is>
2026-04-15 14:27:18 +0200

    fix(backend): Fix passlib bug

--- Backend/app/utils/errors.py
+++ Backend/app/utils/errors.py
@@ -11,6 +11,9 @@ class RoleNotFoundError(Exception):
 class DatabaseTransactionError(Exception):
     pass
 
+class UserNotFoundError(Exception):
+    pass
+
 def setup_exception_handlers(app: FastAPI):
     @app.exception_handler(EmailAlreadyRegisteredError)
     async def email_registered_handler(request: Request, exc: EmailAlreadyRegisteredError):
@@ -42,9 +45,17 @@ def setup_exception_handlers(app: FastAPI):
             # Clean up the "Value error, " prefix from model validators if it exists
             if msg.startswith("Value error, "):
                 msg = msg.replace("Value error, ", "")
-            error_messages.append(msg)
+            error_messages.append(msg)  
             
         return JSONResponse(
             status_code=422,
             content={"detail": " | ".join(error_messages)}
         )
+    
+
+    @app.exception_handler(UserNotFoundError)
+    async def user_not_found_handler(request: Request, exc: UserNotFoundError):
+        return JSONResponse(
+            status_code=404, # Standard za "ne postoji"
+            content={"detail": str(exc)},
+    )
--- Backend/app/utils/security.py
+++ Backend/app/utils/security.py
@@ -1,19 +1,16 @@
+import bcrypt
 from datetime import datetime, timedelta, timezone
-from jose import jwt # type: ignore
-from passlib.context import CryptContext
+from jose import jwt
 from app.config import settings
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    return bcrypt.checkpw(plain_password.encode("utf-8"), hashed_password.encode("utf-8"))
 
 def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
 
 def create_access_token(data: dict) -> str:
     to_encode = data.copy()
-    # Sada pristupamo varijablama preko settings objekta
     expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
@@ -23,6 +20,5 @@ def create_refresh_token(data: dict) -> str:
     expire = datetime.now(timezone.utc) + timedelta(days=settings.REFRESH_TOKEN_EXPIRE_DAYS)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
-
 
 
