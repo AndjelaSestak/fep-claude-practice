1e15b58d5c6256d9dcbd149d3ad73b0a23e9b592
Lazar-Mrdjenovic <lazar.mrdjenovic@symphony.is>
2026-05-08 14:45:27 +0200

    fix(frontend): Fixed my route in index.jsx

--- Frontend/my-app/src/router/index.jsx
+++ Frontend/my-app/src/router/index.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom'
-import HomePage from '../pages/HomePage'
+import HomePage from '../pages/home/HomePage'
 import LoginPage from '../pages/login/LoginPage'
 import NotFoundPage from '../pages/NotFoundPage'
 import DashboardPage from '../pages/DashboardPage'
