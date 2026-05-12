e3967fe7b458ed9729b61e95a8769888a63e6e27
Vladimir Josic <vladimir.josic@symphony.com>
2026-05-12 10:57:02 +0200

    fix(frontend):Add toast message

--- Frontend/my-app/src/pages/forgot-password/components/ForgotPasswordForm.jsx
+++ Frontend/my-app/src/pages/forgot-password/components/ForgotPasswordForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { toast } from 'react-toastify'
 import { useMail } from '../../../hooks/useMail'
 import Button from '../../../components/ui/Button'
 import FormField from '../../../components/ui/FormField'
@@ -21,6 +22,7 @@ const ForgotPasswordForm = () => {
         fieldErrors[err.path[0]] = err.message
       })
       setErrors(fieldErrors)
+      toast.error('Please fix the errors before continuing.')
       return
     }
 
