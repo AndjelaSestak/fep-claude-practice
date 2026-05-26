39aa9f5b283259fefd681cdc35a8190bea9287fa
Vladimir Josic <vladimir.josic@symphony.com>
2026-05-26 11:05:44 +0200

    fix(backend):Refactor Auth service and Router. Added necesery repositories.

--- Backend/alembic/versions/95fb82112965_make_datetime_columns_timezone_aware.py
+++ Backend/alembic/versions/95fb82112965_make_datetime_columns_timezone_aware.py
@@ -5,6 +5,7 @@ Revises: 08c75ee0e9d6
 Create Date: 2026-04-28 10:59:31.785314
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,135 +13,207 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision: str = '95fb82112965'
-down_revision: Union[str, Sequence[str], None] = '08c75ee0e9d6'
+revision: str = "95fb82112965"
+down_revision: Union[str, Sequence[str], None] = "08c75ee0e9d6"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
     """Upgrade schema."""
-    op.alter_column('card_reports', 'created_at',
-               existing_type=postgresql.TIMESTAMP(),
-               type_=sa.DateTime(timezone=True),
-               existing_nullable=False,
-               postgresql_using="created_at AT TIME ZONE 'UTC'")
-    op.alter_column('cards', 'created_at',
-               existing_type=postgresql.TIMESTAMP(),
-               type_=sa.DateTime(timezone=True),
-               existing_nullable=False,
-               postgresql_using="created_at AT TIME ZONE 'UTC'")
-    op.alter_column('email_verifications', 'expires_at',
-               existing_type=postgresql.TIMESTAMP(),
-               type_=sa.DateTime(timezone=True),
-               existing_nullable=False,
-               postgresql_using="expires_at AT TIME ZONE 'UTC'")
-    op.alter_column('recurring_transactions', 'next_run_at',
-               existing_type=postgresql.TIMESTAMP(),
-               type_=sa.DateTime(timezone=True),
-               existing_nullable=True,
-               postgresql_using="next_run_at AT TIME ZONE 'UTC'")
-    op.alter_column('refresh_tokens', 'expires_at',
-               existing_type=postgresql.TIMESTAMP(),
-               type_=sa.DateTime(timezone=True),
-               existing_nullable=False,
-               postgresql_using="expires_at AT TIME ZONE 'UTC'")
-    op.alter_column('refresh_tokens', 'created_at',
-               existing_type=postgresql.TIMESTAMP(),
-               type_=sa.DateTime(timezone=True),
-               existing_nullable=False,
-               postgresql_using="created_at AT TIME ZONE 'UTC'")
-    op.alter_column('transaction_templates', 'created_at',
-               existing_type=postgresql.TIMESTAMP(),
-               type_=sa.DateTime(timezone=True),
-               existing_nullable=False,
-               postgresql_using="created_at AT TIME ZONE 'UTC'")
-    op.alter_column('transaction_templates', 'updated_at',
-               existing_type=postgresql.TIMESTAMP(),
-               type_=sa.DateTime(timezone=True),
-               existing_nullable=False,
-               postgresql_using="updated_at AT TIME ZONE 'UTC'")
-    op.alter_column('transactions', 'created_at',
-               existing_type=postgresql.TIMESTAMP(),
-               type_=sa.DateTime(timezone=True),
-               existing_nullable=False,
-               postgresql_using="created_at AT TIME ZONE 'UTC'")
-    op.alter_column('users', 'created_at',
-               existing_type=postgresql.TIMESTAMP(),
-               type_=sa.DateTime(timezone=True),
-               existing_nullable=False,
-               postgresql_using="created_at AT TIME ZONE 'UTC'")
-    op.alter_column('wallets', 'created_at',
-               existing_type=postgresql.TIMESTAMP(),
-               type_=sa.DateTime(timezone=True),
-               existing_nullable=False,
-               postgresql_using="created_at AT TIME ZONE 'UTC'")
-    op.alter_column('wallets', 'updated_at',
-               existing_type=postgresql.TIMESTAMP(),
-               type_=sa.DateTime(timezone=True),
-               existing_nullable=False,
-               postgresql_using="updated_at AT TIME ZONE 'UTC'")
+    op.alter_column(
+        "card_reports",
+        "created_at",
+        existing_type=postgresql.TIMESTAMP(),
+        type_=sa.DateTime(timezone=True),
+        existing_nullable=False,
+        postgresql_using="created_at AT TIME ZONE 'UTC'",
+    )
+    op.alter_column(
+        "cards",
+        "created_at",
+        existing_type=postgresql.TIMESTAMP(),
+        type_=sa.DateTime(timezone=True),
+        existing_nullable=False,
+        postgresql_using="created_at AT TIME ZONE 'UTC'",
+    )
+    op.alter_column(
+        "email_verifications",
+        "expires_at",
+        existing_type=postgresql.TIMESTAMP(),
+        type_=sa.DateTime(timezone=True),
+        existing_nullable=False,
+        postgresql_using="expires_at AT TIME ZONE 'UTC'",
+    )
+    op.alter_column(
+        "recurring_transactions",
+        "next_run_at",
+        existing_type=postgresql.TIMESTAMP(),
+        type_=sa.DateTime(timezone=True),
+        existing_nullable=True,
+        postgresql_using="next_run_at AT TIME ZONE 'UTC'",
+    )
+    op.alter_column(
+        "refresh_tokens",
+        "expires_at",
+        existing_type=postgresql.TIMESTAMP(),
+        type_=sa.DateTime(timezone=True),
+        existing_nullable=False,
+        postgresql_using="expires_at AT TIME ZONE 'UTC'",
+    )
+    op.alter_column(
+        "refresh_tokens",
+        "created_at",
+        existing_type=postgresql.TIMESTAMP(),
+        type_=sa.DateTime(timezone=True),
+        existing_nullable=False,
+        postgresql_using="created_at AT TIME ZONE 'UTC'",
+    )
+    op.alter_column(
+        "transaction_templates",
+        "created_at",
+        existing_type=postgresql.TIMESTAMP(),
+        type_=sa.DateTime(timezone=True),
+        existing_nullable=False,
+        postgresql_using="created_at AT TIME ZONE 'UTC'",
+    )
+    op.alter_column(
+        "transaction_templates",
+        "updated_at",
+        existing_type=postgresql.TIMESTAMP(),
+        type_=sa.DateTime(timezone=True),
+        existing_nullable=False,
+        postgresql_using="updated_at AT TIME ZONE 'UTC'",
+    )
+    op.alter_column(
+        "transactions",
+        "created_at",
+        existing_type=postgresql.TIMESTAMP(),
+        type_=sa.DateTime(timezone=True),
+        existing_nullable=False,
+        postgresql_using="created_at AT TIME ZONE 'UTC'",
+    )
+    op.alter_column(
+        "users",
+        "created_at",
+        existing_type=postgresql.TIMESTAMP(),
+        type_=sa.DateTime(timezone=True),
+        existing_nullable=False,
+        postgresql_using="created_at AT TIME ZONE 'UTC'",
+    )
+    op.alter_column(
+        "wallets",
+        "created_at",
+        existing_type=postgresql.TIMESTAMP(),
+        type_=sa.DateTime(timezone=True),
+        existing_nullable=False,
+        postgresql_using="created_at AT TIME ZONE 'UTC'",
+    )
+    op.alter_column(
+        "wallets",
+        "updated_at",
+        existing_type=postgresql.TIMESTAMP(),
+        type_=sa.DateTime(timezone=True),
+        existing_nullable=False,
+        postgresql_using="updated_at AT TIME ZONE 'UTC'",
+    )
 
 
 def downgrade() -> None:
     """Downgrade schema."""
-    op.alter_column('wallets', 'updated_at',
-               existing_type=sa.DateTime(timezone=True),
-               type_=postgresql.TIMESTAMP(),
-               existing_nullable=False,
-               postgresql_using="updated_at AT TIME ZONE 'UTC'")
-    op.alter_column('wallets', 'created_at',
-               existing_type=sa.DateTime(timezone=True),
-               type_=postgresql.TIMESTAMP(),
-               existing_nullable=False,
-               postgresql_using="created_at AT TIME ZONE 'UTC'")
-    op.alter_column('users', 'created_at',
-               existing_type=sa.DateTime(timezone=True),
-               type_=postgresql.TIMESTAMP(),
-               existing_nullable=False,
-               postgresql_using="created_at AT TIME ZONE 'UTC'")
-    op.alter_column('transactions', 'created_at',
-               existing_type=sa.DateTime(timezone=True),
-               type_=postgresql.TIMESTAMP(),
-               existing_nullable=False,
-               postgresql_using="created_at AT TIME ZONE 'UTC'")
-    op.alter_column('transaction_templates', 'updated_at',
-               existing_type=sa.DateTime(timezone=True),
-               type_=postgresql.TIMESTAMP(),
-               existing_nullable=False,
-               postgresql_using="updated_at AT TIME ZONE 'UTC'")
-    op.alter_column('transaction_templates', 'created_at',
-               existing_type=sa.DateTime(timezone=True),
-               type_=postgresql.TIMESTAMP(),
-               existing_nullable=False,
-               postgresql_using="created_at AT TIME ZONE 'UTC'")
-    op.alter_column('refresh_tokens', 'created_at',
-               existing_type=sa.DateTime(timezone=True),
-               type_=postgresql.TIMESTAMP(),
-               existing_nullable=False,
-               postgresql_using="created_at AT TIME ZONE 'UTC'")
-    op.alter_column('refresh_tokens', 'expires_at',
-               existing_type=sa.DateTime(timezone=True),
-               type_=postgresql.TIMESTAMP(),
-               existing_nullable=False,
-               postgresql_using="expires_at AT TIME ZONE 'UTC'")
-    op.alter_column('recurring_transactions', 'next_run_at',
-               existing_type=sa.DateTime(timezone=True),
-               type_=postgresql.TIMESTAMP(),
-               existing_nullable=True,
-               postgresql_using="next_run_at AT TIME ZONE 'UTC'")
-    op.alter_column('email_verifications', 'expires_at',
-               existing_type=sa.DateTime(timezone=True),
-               type_=postgresql.TIMESTAMP(),
-               existing_nullable=False,
-               postgresql_using="expires_at AT TIME ZONE 'UTC'")
-    op.alter_column('cards', 'created_at',
-               existing_type=sa.DateTime(timezone=True),
-               type_=postgresql.TIMESTAMP(),
-               existing_nullable=False,
-               postgresql_using="created_at AT TIME ZONE 'UTC'")
-    op.alter_column('card_reports', 'created_at',
-               existing_type=sa.DateTime(timezone=True),
-               type_=postgresql.TIMESTAMP(),
-               existing_nullable=False,
-               postgresql_using="created_at AT TIME ZONE 'UTC'")
+    op.alter_column(
+        "wallets",
+        "updated_at",
+        existing_type=sa.DateTime(timezone=True),
+        type_=postgresql.TIMESTAMP(),
+        existing_nullable=False,
+        postgresql_using="updated_at AT TIME ZONE 'UTC'",
+    )
+    op.alter_column(
+        "wallets",
+        "created_at",
+        existing_type=sa.DateTime(timezone=True),
+        type_=postgresql.TIMESTAMP(),
+        existing_nullable=False,
+        postgresql_using="created_at AT TIME ZONE 'UTC'",
+    )
+    op.alter_column(
+        "users",
+        "created_at",
+        existing_type=sa.DateTime(timezone=True),
+        type_=postgresql.TIMESTAMP(),
+        existing_nullable=False,
+        postgresql_using="created_at AT TIME ZONE 'UTC'",
+    )
+    op.alter_column(
+        "transactions",
+        "created_at",
+        existing_type=sa.DateTime(timezone=True),
+        type_=postgresql.TIMESTAMP(),
+        existing_nullable=False,
+        postgresql_using="created_at AT TIME ZONE 'UTC'",
+    )
+    op.alter_column(
+        "transaction_templates",
+        "updated_at",
+        existing_type=sa.DateTime(timezone=True),
+        type_=postgresql.TIMESTAMP(),
+        existing_nullable=False,
+        postgresql_using="updated_at AT TIME ZONE 'UTC'",
+    )
+    op.alter_column(
+        "transaction_templates",
+        "created_at",
+        existing_type=sa.DateTime(timezone=True),
+        type_=postgresql.TIMESTAMP(),
+        existing_nullable=False,
+        postgresql_using="created_at AT TIME ZONE 'UTC'",
+    )
+    op.alter_column(
+        "refresh_tokens",
+        "created_at",
+        existing_type=sa.DateTime(timezone=True),
+        type_=postgresql.TIMESTAMP(),
+        existing_nullable=False,
+        postgresql_using="created_at AT TIME ZONE 'UTC'",
+    )
+    op.alter_column(
+        "refresh_tokens",
+        "expires_at",
+        existing_type=sa.DateTime(timezone=True),
+        type_=postgresql.TIMESTAMP(),
+        existing_nullable=False,
+        postgresql_using="expires_at AT TIME ZONE 'UTC'",
+    )
+    op.alter_column(
+        "recurring_transactions",
+        "next_run_at",
+        existing_type=sa.DateTime(timezone=True),
+        type_=postgresql.TIMESTAMP(),
+        existing_nullable=True,
+        postgresql_using="next_run_at AT TIME ZONE 'UTC'",
+    )
+    op.alter_column(
+        "email_verifications",
+        "expires_at",
+        existing_type=sa.DateTime(timezone=True),
+        type_=postgresql.TIMESTAMP(),
+        existing_nullable=False,
+        postgresql_using="expires_at AT TIME ZONE 'UTC'",
+    )
+    op.alter_column(
+        "cards",
+        "created_at",
+        existing_type=sa.DateTime(timezone=True),
+        type_=postgresql.TIMESTAMP(),
+        existing_nullable=False,
+        postgresql_using="created_at AT TIME ZONE 'UTC'",
+    )
+    op.alter_column(
+        "card_reports",
+        "created_at",
+        existing_type=sa.DateTime(timezone=True),
+        type_=postgresql.TIMESTAMP(),
+        existing_nullable=False,
+        postgresql_using="created_at AT TIME ZONE 'UTC'",
+    )
